eec12916440425660d2536f823541874da037993
xinjuejianshen <118965698+xinjuejianshen@users.noreply.github.com>
2024-11-14 22:00:36 +0800

    Add files via upload

---
+++ 01_01.py
@@ -0,0 +1,7 @@
+year=input('输入当前年：')
+month=input('输入当前月：')
+date=input('输入当前日：')
+print(year,month,date,sep='-')
+print(year+'年'+month+'月'+date+'日')
+print('{}年{}月{}日'.format(year,month,date))
+print(f'{year}年{month}月{date}日')
